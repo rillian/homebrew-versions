970513d19238a9c0ad02951f7bf5f7d80f779073
Ralph Giles <giles@mozilla.com>
2013-07-18 13:21:17 -0700

    Add libpng 1.6.3.
    This version provides some newer api calls.

---
+++ libpng16.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Libpng < Formula
+  homepage 'http://www.libpng.org/pub/png/libpng.html'
+  url 'http://downloads.sf.net/project/libpng/libpng16/1.6.3/libpng-1.6.3.tar.gz'
+  sha1 'b8b7b911909c09d71324536aaa7750104d170c77'
+
+  keg_only :provided_pre_mountain_lion
+
+  option :universal
+
+  def install
+    ENV.universal_binary if build.universal?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
